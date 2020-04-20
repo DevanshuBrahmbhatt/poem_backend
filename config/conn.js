a05c4b3f8dc4deeda0f2df1d6e4b21bf3aca03e9
Devanshu Brahmbhatt <devanshu.vguj@gmail.com>
2020-04-20 14:13:41 +0530

    Update conn.js

--- config/conn.js
+++ config/conn.js
@@ -1,7 +1,1 @@
-module.exports={
- url :'mongodb+srv://devanshu1128:TQ1WlRhGsNDFldHl@devanshucreation-id5ah.mongodb.net/test?retryWrites=true&w=majority',
- secret:"itismydarksecdd1128",
- username:"dev123",
- password:"1234567"
-};
 
